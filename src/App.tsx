286f4979f94c32f54af6357cd5685cc7af64bca1
GuitherezDika <guitherez.git@gmail.com>
2025-11-13 17:28:42 +0700

    React Basic Refs: #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+interface MyComponentProps {
+  name: string;
+}
+const MyComponent: React.FC<MyComponentProps> = ({name}) => {
+  return (
+    <div>
+      <h1>Hello World1 {name}!</h1>
+      <p>Paragraph Component</p>
+    </div>
+  )
+}
+
+const Counter: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const increment = () => {
+    setCount(prevState => prevState + 1);
+  };
+
+  return (
+    <div>
+      <p>Count {count}</p>
+      <button  onClick={increment} >Count Me</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, World!</h1>
+        <p>This is a paragraph</p>
+        <MyComponent name='aaaa' />
+        <Counter />
       </header>
+
     </div>
   );
 }
